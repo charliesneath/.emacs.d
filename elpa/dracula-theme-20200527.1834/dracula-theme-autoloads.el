bef2f22053de51ef5490e4956c92b57089c6146d
Charlie <charlie.sneath@gmail.com>
2020-06-11 08:06:12 -0400

    files for emacs config

---
+++ elpa/dracula-theme-20200527.1834/dracula-theme-autoloads.el
@@ -0,0 +1,24 @@
+;;; dracula-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "dracula-theme" "dracula-theme.el" (24290 7065
+;;;;;;  543754 143000))
+;;; Generated autoloads from dracula-theme.el
+
+(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;;;### (autoloads nil nil ("dracula-theme-pkg.el" "test-profile.el")
+;;;;;;  (24290 7065 573754 210000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; dracula-theme-autoloads.el ends here
---
+++ init.el
@@ -0,0 +1,51 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(desktop-save-mode 1)
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("dcdd1471fde79899ae47152d090e3551b889edf4b46f00df36d653adc2bf550d" default)))
+ '(package-selected-packages
+   (quote
+    (dracula-theme magit transient-dwim gnu-elpa-keyring-update with-editor transient dash)))
+ '(proced-auto-update-flag t)
+ '(proced-filter (quote sudo)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(font-lock-function-name-face ((t (:foreground "brightyellow")))))
+
+(global-set-key (kbd "C-x g") 'magit-status)
+
+;; Use M-arrow to change current window
+(windmove-default-keybindings 'meta)
+
+;; source: http://steve.yegge.googlepages.com/my-dot-emacs-file
+(defun rename-file-and-buffer (new-name)
+  "Renames both current buffer and file it's visiting to NEW-NAME."
+  (interactive "sNew name: ")
+  (let ((name (buffer-name))
+        (filename (buffer-file-name)))
+    (if (not filename)
+        (message "Buffer '%s' is not visiting a file!" name)
+      (if (get-buffer new-name)
+          (message "A buffer named '%s' already exists!" new-name)
+        (progn
+          (rename-file filename new-name 1)
+          (rename-buffer new-name)
+          (set-visited-file-name new-name)
+          (set-buffer-modified-p nil))))))
