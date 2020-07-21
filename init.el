c5dbd775bc16e470f903d3e8ab8903401f6d44f6
Charlie Sneath <charlie.sneath@gmail.com>
2020-07-21 09:48:07 -0400

    Update init.el
    reverting default background and foreground color settings

--- init.el
+++ init.el
@@ -155,4 +155,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#282a36" :foreground "#f8f8f2" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 160 :width normal :foundry "nil" :family "PragmataPro Mono Liga")))))
+ '(default ((t (:inherit nil :stipple nil :background nil :foreground nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 160 :width normal :foundry "nil" :family "PragmataPro Mono Liga")))))
